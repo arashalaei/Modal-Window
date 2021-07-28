85f99c867a5499e2470df7756c8f872d93711a44
Arash Alaei <arashalaei@aut.ac.ir>
2021-07-28 21:10:38 +0430

    done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const className = {
     modal:'.modal',
     overlay: '.overlay',
     btnCloseModal: '.close-modal',
-    btnsOpenModal: '.show-moda'
+    btnsOpenModal: '.show-modal'
 }
 
 const elements = {
@@ -16,3 +16,31 @@ const elements = {
     btnCloseModal: document.querySelector(className.btnCloseModal),
     btnsOpenModal: document.querySelectorAll(className.btnsOpenModal)
 }
+
+/**
+    *** Controllers ***  
+ */
+
+const handlers = {
+    openModal: function(){
+        elements.modal.classList.remove('hidden');
+        elements.overlay.classList.remove('hidden')
+    },
+
+    closeModal:function(){
+        elements.modal.classList.add('hidden');
+        elements.overlay.classList.add('hidden')
+    },
+
+    escHandlere:function(e){
+        if((e.which === 27|| e.code === 'Escape') && (!elements.modal.classList.contains('hidden')))
+                handlers.closeModal()
+    }
+}
+
+for(let i = 0; i < elements.btnsOpenModal.length; i++)
+    elements.btnsOpenModal[i].addEventListener('click', handlers.openModal)
+
+elements.btnCloseModal.addEventListener('click',handlers.closeModal)
+elements.overlay.addEventListener('click', handlers.closeModal)
+document.addEventListener('keydown', handlers.escHandlere)
